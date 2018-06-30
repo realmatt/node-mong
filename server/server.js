276b6a9cd936809424526b6ca762d48ea7e2dfcf
Mustafa <mustafa@kodency.com>
2018-07-01 01:58:59 +0300

    some change

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {Users} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -16,20 +18,36 @@ app.post('/todos',(req,res)=>{
         res.status(200).send(doc);
     },(e)=>{
         res.status(400).send(e);
-        console.log(e);
+        //console.log(e);
     });
 });
 
 app.get('/todos',(req,res)=>{
-    Todo.find().then((doc)=>{
-        res.send({doc});
+    Todo.find().then((todos)=>{
+        res.send({todos});
     },(e)=>{
         res.status(400).send(e);
     })
 });
 
-app.listen(3000, ()=>{
-   console.log('Server is listening on Port 3000'); 
+app.get('/todos/:id',(req,res)=>{
+    var id =req.params.id;    
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }    
+    Todo.findById({_id:id}).then((todos)=>{
+        //console.log(todos);
+        if(!todos){
+            return res.status(404).send();
+        }
+        res.send({todos})
+    }).catch((e)=>{
+        res.status(400).send(e);
+    });
+});
+
+app.listen(port, ()=>{
+   console.log('Server is listening on Port ',port); 
 });
 
 module.exports = {app};
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,27 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectId} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [
+    {   _id:new ObjectId(),
+        text:'Todo1'
+    }, 
+    {
+        _id:new ObjectId(),
+        text:'Todo2'
+    }];
+
+var id = '';
+
 /* Remove all the elements in collection */
 beforeEach((done)=>{
     Todo.remove({}).then(()=>{
+        return Todo.insertMany(todos);       
+    }).then((res)=>{
+        //console.log(res); 
         done();
     })
 });
@@ -29,7 +44,7 @@ describe('POST /todos',()=>{
                 return done(err);
             }
             //check also db if it exist there
-            Todo.find({}).then((todos)=>{
+            Todo.find({text}).then((todos)=>{
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -52,10 +67,60 @@ describe('POST /todos',()=>{
             }
             // check db if none exist in db
             Todo.find({}).then((todos)=>{
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
             }).catch((e)=>done(e));
         })
     });
 
 });
+
+describe('GET /todos',()=>{
+    it('should get all todos',(done)=>{
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res)=>{
+            //console.log(res.body);
+            expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
+    });
+});
+
+describe('^GET Ind. /todos',()=>{
+    it('should return the todo by id',(done)=>{
+        //console.log(id);
+        //id icin todos[0].toHexString() can be used
+        request(app)
+        .get('/todos/'+todos[0]._id.toHexString())
+        .expect(200)
+        .expect((res)=>{
+            //console.log(res.body);
+            expect(res.body.todos.text).toExist(todos[0].text);
+        })
+        .end(done); 
+    });
+
+    it('should return 404 because of invalid id',(done)=>{
+        request(app)
+        .get('/todos/123')
+        .expect(404)
+        .expect((res)=>{
+            expect(res.body).toEqual({});
+        })
+        .end(done);
+    });
+
+    it('should return 404 because of not exist in db',(done)=>{
+        var id1 = new ObjectId().toHexString();      
+    request(app)
+        .get('/todos/'+id1)
+        .expect(404)
+        .expect((res)=>{
+            expect(res.body).toEqual({});
+        })
+        .end(done);
+    });
+ 
+})
