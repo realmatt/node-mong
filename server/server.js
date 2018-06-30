a85a11e70fc374f45ae93aa46168fcb8d00dff34
Mustafa <mustafa@kodency.com>
2018-06-30 14:18:09 +0300

    pots endpoint added

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {Users} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+    console.log(req.body);
+    var todo = new Todo(req.body);
+    todo.save().then((doc)=>{
+        res.status(200).send(doc);
+    },(e)=>{
+        res.status(400).send(e);
+        console.log(e);
+    });
+});
+
+
+
+app.listen(3000, ()=>{
+   console.log('Server is listening on Port 3000'); 
+});
+
